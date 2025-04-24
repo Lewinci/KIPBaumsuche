206fc7b40bc280d8617c3311c6edc921d535cdbb
Lewinci <Paul.Fischer2@haw-hamburg.de>
2025-04-24 16:22:45 +0200

    created Maze methods

--- Maze.py
+++ Maze.py
@@ -1,20 +1,20 @@
-import random
 class Maze:
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
         #2D Array wird vorerst mit Nullen gefüllt
-        self.grid = [[0 for _ in range(width)] for _ in range(height)]
+        self.grid = [[1 for _ in range(width)] for _ in range(height)]
 
         self.start = (0, 0)
-        self.goal = (width, height)
+        self.goal = (width -1, height -1)
+
 
     def set_start(self, x, y):
         self.start = (x, y)
 
     def set_goal(self, x, y):
-        self.goal = (x, y)
+        self.goal = (x - 1, y - 1)
 
     def set_obstacle(self, x, y):
         check_obstacle_position = (x,y)
@@ -29,7 +29,21 @@ class Maze:
 
     def set_path(self, x, y):
         if 0 <= x < self.width and 0 <= y < self.height:
-            self.grid[x][y] = 1
+            self.grid[y][x] = 0
         else:
             raise ValueError(f"Position ({x}, {y}) out of bounds.")
 
+    def print_maze(self):
+        for y in range(self.height):
+            row = ""
+            for x in range(self.width):
+                pos = (x, y)
+                if pos == self.start:
+                    row += "S "
+                elif pos == self.goal:
+                    row += "G "
+                elif self.grid[y][x] == 1:
+                    row += "█ "
+                else:
+                    row += ". "
+            print(row)
---
+++ Mazegenerator.py
@@ -0,0 +1,74 @@
+import random
+from Maze import Maze
+
+#MIN_MANHATTEN_DISTANCE = 1
+
+def manhattan_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def generate_maze_with_eller(maze_width, maze_height):
+    assert 0 < maze_width <= 100
+    assert 0 < maze_height <= 100
+    #assert 0 < min_man_distance
+    #assert min_man_distance <= max_man_distance <= (maze_width + maze_height)
+
+
+    width = maze_width
+    height = maze_height
+
+    maze = Maze(width, height)
+
+    # Initialize each cell with a unique set
+    sets = [i for i in range(width)]
+    grid = [[0 for _ in range(width)] for _ in range(height)]
+    next_set = width
+
+    for y in range(height):
+        # JOIN RIGHT CELLS
+        for x in range(width - 1):
+            if sets[x] != sets[x + 1] and (random.random() < 0.5 or y == height - 1):
+                old_set, new_set = sets[x + 1], sets[x]
+                for i in range(width):
+                    if sets[i] == old_set:
+                        sets[i] = new_set
+                grid[y][x + 1] = 1  # remove wall
+
+        # CREATE VERTICAL CONNECTIONS
+        new_sets = [-1] * width
+        for set_id in set(sets):
+            members = [i for i, s in enumerate(sets) if s == set_id]
+            num_verticals = max(1, random.randint(1, len(members)))
+            verticals = random.sample(members, num_verticals)
+            for x in verticals:
+                if y + 1 < height:
+                    grid[y + 1][x] = 1  # remove wall
+                    new_sets[x] = set_id
+
+        # Update sets for next row
+        for x in range(width):
+            if new_sets[x] == -1:
+                new_sets[x] = next_set
+                next_set += 1
+        sets = new_sets
+
+        # Mark all paths
+        for x in range(width):
+            if grid[y][x] == 1:
+                maze.set_path(x, y)
+
+    # Erzeuge gültige Start- und Zielposition mit Mindestdistanz
+    #valid_positions = [(x, y) for y in range(height) for x in range(width) if grid[y][x] == 1]
+    #while True:
+    #    start = random.choice(valid_positions)
+    #    goal = random.choice(valid_positions)
+    #    if max_man_distance >= manhattan_distance(start, goal) >= min_man_distance:
+    #        break
+
+    #maze.set_start(*start)
+    #maze.set_goal(goal[0] + 1, goal[1] + 1)  # wie in set_goal mit -1 Korrektur'
+
+    return maze
+
+
+maze = generate_maze_with_eller(50, 50)
+maze.print_maze()
